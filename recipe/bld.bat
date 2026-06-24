9d2ea2ca0133008cb2b2246bdba49402fea5e1e7
Andrii Osipov <aosipov@anaconda.com>
2026-06-24 04:59:08 -0700

    fix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,7 @@
 @echo on
 
-:: CPython 3.9+ Windows x86-64
+:: Dead code for Windows Arm x86-64 on future builds when WIN ARM is available
+:: CPython 3.9+ Windows Arm x86-64
 %PYTHON% -m pip install --no-deps --no-build-isolation https://pypi.org/packages/cp39/r/runtimed/runtimed-%PKG_VERSION%-cp39-cp39-win_amd64.whl
 
 if errorlevel 1 exit 1

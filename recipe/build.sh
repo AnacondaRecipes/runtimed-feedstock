ba649d91374e42f581f8e8dd35244e0965ccab93
Andrii Osipov <aosipov@anaconda.com>
2026-06-24 05:07:31 -0700

    fix

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,6 +12,7 @@ if [ `uname` == Darwin ]; then
     curl -Lso "$WHL_FILE" https://pypi.org/packages/cp39/r/runtimed/runtimed-${PKG_VERSION}-cp39-abi3-macosx_11_0_arm64.whl
 fi
 
+# Dead code for Linux x86-64 on future builds when MANYLINUX is available
 if [ `uname` == Linux ]; then
     # CPython 3.9+ manylinux: glibc 2.39+ x86-64
     WHL_FILE=https://pypi.org/packages/cp39/r/runtimed/runtimed-${PKG_VERSION}-cp39-abi3-manylinux_2_39_x86_64.whl
